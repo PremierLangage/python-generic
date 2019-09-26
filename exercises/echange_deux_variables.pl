60e293ef07e16d1c02cf4267727afbfee34a6536
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-09-26 16:01:17 +0200

    Update echange_deux_variables.pl
    Push the latest version from PL

--- exercises/echange_deux_variables.pl
+++ exercises/echange_deux_variables.pl
@@ -1,29 +1,40 @@
 # author = Antoine Meyer
-extends = ../templates/ap1_template.pl
+extends = ../templates/generic/generic.pl
+
 title = Échange de deux variables
 
 text==
-On suppose qu'il existe deux variables `a` et `b` de valeurs et de types
+On suppose qu'il existe deux variables `a` et `b` de valeurs et de types 
 quelconques précédemment fixées (vous n'avez donc pas à les initialiser
 ni à les lire).
 
-Écrire un programme dont l'exécution échange les valeurs de `a` et `b`,
+Écrire un programme dont l'exécution échange les valeurs de `a` et `b`, 
 sans rien afficher.
 
-**Attention**, *votre programe doit fonctionner quelles que soient les valeurs
+**Attention**, *votre programe doit fonctionner quelles que soient les valeurs 
 de `a` et `b` et leurs types (qui peuvent être différents).*<br><br>
 ==
 
 grader==
-begin_test_group("Échange de deux entiers")
-set_globals(a=1, b=2)
-run()
-assert_variable_values(a=2, b=1)
-end_test_group()
+run(
+    title='Deux nombres',
+    globals={'a': 1, 'b': 2}, 
+    values={'a': 2, 'b': 1}
+)
 
-begin_test_group("Échange d'un entier et d'une chaîne")
-set_globals(a=1, b="ça marche")
-run()
-assert_variable_values(a="ça marche", b=1)
-end_test_group()
+run(
+    title='Nombre et chaîne',
+    globals={'a': 1, 'b': 'ça marche !'},
+    values={'a': 'ça marche !', 'b': 1}
+)
+
+begin_test_group("Nombres aléatoires")
+from random import randrange
+for _ in range(5):
+    x, y = randrange(0, 1000), randrange(1000, 2000)
+    run(
+        title='Nombres aléatoires',
+        globals={'a': x, 'b': y},
+        values={'a': y, 'b': x}
+    )
 ==
