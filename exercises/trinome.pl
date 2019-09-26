0d574aa84d6875361b3cd6690906eff29b5a52c5
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-09-26 16:03:09 +0200

    Added random trinomial to avoid hardcoded solution

--- exercises/trinome.pl
+++ exercises/trinome.pl
@@ -1,20 +1,22 @@
 # author = Antoine Meyer
-extends = ../templates/ap1_template.pl
+extends = ../templates/generic/generic.pl
 title = Trinôme du second degré
 
 text==
-On suppose qu'il existe trois variables `a`, `b` et 'c' de valeurs `int` ou `float`.
-Écrire un programme dont l'exécution crée une variable `nb_solutions` dont la valeur
-représente le nombre de solutions réelles de l'équation $a x^2 + b x + c$.
+On suppose qu'il existe trois variables `a`, `b` et `c` de valeurs `int` ou `float`.
+Écrire un programme dont l'exécution crée une variable `nb_solutions` dont la valeur 
+représente le nombre de solutions réelles de l'équation $%a x^2 + b x + c%$.
 
-On représentera le fait que l'équation à une infinité de solutions en affectant à
-la variable `nb_solutions` la valeur `float('inf')` (qui représente $+\infty$).
+On représentera le fait que l'équation a une infinité de solutions en affectant à 
+la variable `nb_solutions` la valeur `float('inf')` (qui représente $%+\infty%$).
 
-**Attention**, *votre programe doit fonctionner quelles que soient les valeurs
+**Attention**, *votre programme doit fonctionner quelles que soient les valeurs 
 de `a`, `b` et `c`.*<br><br>
 ==
 
 grader==
+import random
+
 def nb_sols(a, b, c):
     if a == 0:
         if b == 0:
@@ -33,12 +35,20 @@ def nb_sols(a, b, c):
         return 0
 
 essais = [
-    [0, 0, 0], [0, 0, 1], [0, 1, 1],
-    [1, 3, 1], [1, 2, 1], [1, 1, 1]
+    [0, 0, 0], [0, 0, 1], [0, 1, 1], 
+    [1, 3, 1], [1, 2, 1], [1, 1, 1],
+    [
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.randint(0, 10)
+    ]
 ]
 
+essais += []
+
+begin_test_group("Résultats")
 for a, b, c in essais:
     set_globals(a=a, b=b, c=c)
-    run()
+    run(title="$%{} x^2 + {} x + c = 0%$".format(a, b, c))
     assert_variable_values(nb_solutions=nb_sols(a, b, c))
 ==
